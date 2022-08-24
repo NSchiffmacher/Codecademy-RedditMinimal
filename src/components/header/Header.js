ae1face554c255441b20eabc9b0476132f2783ca
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-08-24 23:16:22 +0200

    Creating the header

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export function Header() {
+    return (<header>
+        <div className="name">
+            <img src="/logo.png" alt="logo" />
+            <span className="blue">Reddit</span>Minimal
+        </div>
+    </header>)
+}
